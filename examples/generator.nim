29e4d5f62bdb99937b50c93729443b4224f2e4f6
Yoshihiro Tanaka <contact@cordea.jp>
2019-07-13 21:26:12 +0900

    add example

---
+++ examples/generator.nim
@@ -0,0 +1,26 @@
+# Copyright 2019 Yoshihiro Tanaka
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+  # http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Yoshihiro Tanaka <contact@cordea.jp>
+# date  : 2019-07-13
+
+import ../src/hotp
+
+const Secret = "01234567890123456789"
+
+# Base32 encoded secret
+# Used in google authenticator.
+const EncodedSecret = "GAYTEMZUGU3DOOBZGAYTEMZUGU3DOOBZ"
+
+for i in 1..10:
+  echo generate(Secret, i, 6)
